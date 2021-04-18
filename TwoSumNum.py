c66f2b92b783a6b0fd5c16db9fa1e8e5b288d85b
Oladimeji Williams <81469074+Oladimeji-Williams@users.noreply.github.com>
2021-04-18 03:04:01 +0100

    Update TwoSumNum.py

--- TwoSumNum.py
+++ TwoSumNum.py
@@ -5,7 +5,3 @@ def TwoSumNum(arr, a=0):
             if not any(i in sublist for sublist in TwoNum):
                 TwoNum.append([a - i, i])
     return TwoNum
-
-
-c = TwoSumNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 11)
-print(c)
